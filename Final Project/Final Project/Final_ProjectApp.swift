4d313a1bc1c6e91e5275aeffba5465297858a011
if73886 <if73886@uga.edu>
2026-04-13 12:29:52 -0400

    xcode creation

---
+++ Final Project/Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Isabel Finnerty on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
